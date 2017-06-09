c6a1f64298eb9f804d8cb5da9ae753c138a36677
TETO\Teto <joxrios.01@gmail.com>
2017-06-08 21:15:13 -0600

    Primer Commit

---
+++ HT1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.in1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.in2 = new System.Windows.Forms.TextBox();
+            this.in3 = new System.Windows.Forms.TextBox();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // in1
+            // 
+            this.in1.Location = new System.Drawing.Point(12, 39);
+            this.in1.Name = "in1";
+            this.in1.Size = new System.Drawing.Size(100, 20);
+            this.in1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hoja de Trabajo 1";
+            // 
+            // in2
+            // 
+            this.in2.Location = new System.Drawing.Point(152, 39);
+            this.in2.Name = "in2";
+            this.in2.Size = new System.Drawing.Size(100, 20);
+            this.in2.TabIndex = 2;
+            // 
+            // in3
+            // 
+            this.in3.Location = new System.Drawing.Point(300, 39);
+            this.in3.Name = "in3";
+            this.in3.Size = new System.Drawing.Size(100, 20);
+            this.in3.TabIndex = 3;
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(120, 104);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(58, 13);
+            this.Resultado.TabIndex = 4;
+            this.Resultado.Text = "Resultado:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular Hipotenusa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(228, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Verificar Vocales";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(58, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Invertir Nombre";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Regla de 3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 260);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.in3);
+            this.Controls.Add(this.in2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.in1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox in1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox in2;
+        private System.Windows.Forms.TextBox in3;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HT1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(in1.Text);
+            double num2 = Convert.ToDouble(in2.Text);
+            double raiz = Math.Sqrt(Math.Pow(num1, 2) + Math.Pow(num2, 2));
+
+            Resultado.Text = "El resultado es: " + Convert.ToString(raiz);
+        }
+    }
+}
